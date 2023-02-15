cd3180d273e12175d20b92ab7f64952eea31cdb7
hansheng <447725977@qq.com>
2023-02-16 00:53:33 +0800

    feat: add database

--- test/testGorm.go
+++ test/testGorm.go
@@ -1,39 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"go/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
+// type Product struct {
+// 	gorm.Model
+// 	Code  string
+// 	Price uint
+// }
 
 func main() {
-	db, err := gorm.Open(mysql.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/ginchat?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	// 迁移 schema
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&models.UserBasic{})
 
 	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
+	user := &models.UserBasic{}
+	user.Name = "shen"
+	db.Create(user)
 
 	// Read
-	var product Product
-	db.First(&product, 1)                 // 根据整型主键查找
-	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	fmt.Println(db.First(user, 1))
+	// var product Product
+	// db.First(&product, 1)                 // 根据整型主键查找
+	// db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
 
 	// Update - 将 product 的 price 更新为 200
-	db.Model(&product).Update("Price", 200)
+	db.Model(user).Update("PassWord", "1234")
 	// Update - 更新多个字段
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	// db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	// db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
 	// Delete - 删除 product
-	db.Delete(&product, 1)
+	// db.Delete(&product, 1)
 }
