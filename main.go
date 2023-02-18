310dcbfbb1bd0321189bac5d726a2c42e64d8e41
hansheng <447725977@qq.com>
2023-02-19 01:39:05 +0800

    feat: add swagger

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"go/docs"
 	"go/router"
 	"go/utils"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
 	utils.InitConfig()
 	utils.InitMySQL()
 	r := router.Router()
-
-	r.Run(":8080")
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	// 不写127.0.0.1 win每次执行会被防火墙拦截
+	r.Run("127.0.0.1:8080")
 }
--- service/user.go
+++ service/user.go
@@ -6,10 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetUserList
+// @Tags user
+// @Success 200 {string} json{"code", "message"}
+// @Router /user/getUserList [get]
 func GetUserList(c *gin.Context) {
 	data := make([]*models.UserBasic, 10)
 	data = models.GetUserList()
 	c.JSON(200, gin.H{
-		"message": data,
+		"code":    0,
+		"data":    data,
+		"message": "",
 	})
 }
