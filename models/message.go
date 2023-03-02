134f26516d1fb1e02feb5888f7dbc65cfa0c4e67
shenfuyang <shenfuyang@hortorgames.com>
2023-03-02 19:54:53 +0800

    feat: update message

--- models/message.go
+++ models/message.go
@@ -1,20 +1,181 @@
 package models
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"go/utils"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/websocket"
+	"gopkg.in/fatih/set.v0"
 	"gorm.io/gorm"
 )
 
 type Message struct {
 	gorm.Model
-	UserId int64
-	TargetId int64
-	Type int 
-	Media int
-	Content string
-	CreateTime uint64
-	ReadTime uint64
-	Pic string
-	Url string
-	Desc string
-	Amount int
+	UserId     int64  //发送者
+	TargetId   int64  //接受者
+	Type       int    //发送类型  1私聊  2群聊  3心跳
+	Media      int    //消息类型  1文字 2表情包 3语音 4图片 /表情包
+	Content    string //消息内容
+	CreateTime uint64 //创建时间
+	ReadTime   uint64 //读取时间
+	Pic        string
+	Url        string
+	Desc       string
+	Amount     int //其他数字统计
+}
+
+func (table *Message) TableName() string {
+	return "message"
+}
+
+type Node struct {
+	Conn          *websocket.Conn // 连接
+	Addr          string          // 客户端地址
+	FirstTime     uint64          // 首次连接时间
+	HeartbeatTime uint64          // 心跳时间
+	LoginTime     uint64          // 登录时间
+	DataQueue     chan []byte     // 消息
+	GroupSets     set.Interface   // 好友、群
+}
+
+// 映射关系
+var clientMap map[int64]*Node = make(map[int64]*Node, 0)
+
+// 读写锁
+var rwLocker sync.RWMutex
+
+// 需要发送至ID、接受者ID、消息类型、发送的内容、发送类型
+func Chat(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	Id := query.Get("userId")
+	userId, _ := strconv.ParseInt(Id, 10, 64)
+	isValida := true
+	conn, err := (&websocket.Upgrader{
+		// token校验
+		CheckOrigin: func(r *http.Request) bool {
+			return isValida
+		},
+	}).Upgrade(writer, request, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	currentTime := uint64(time.Now().Unix())
+	node := &Node{
+		Conn:          conn,
+		Addr:          conn.RemoteAddr().String(),
+		HeartbeatTime: currentTime,
+		LoginTime:     currentTime,
+		DataQueue:     make(chan []byte, 50),
+		GroupSets:     set.New(set.ThreadSafe),
+	}
+	// 用户关系 userid和node绑定 并加锁
+	rwLocker.Lock()
+	clientMap[userId] = node
+	rwLocker.Unlock()
+
+}
+
+func sendProc(node *Node) {
+	for {
+		select {
+		case data := <-node.DataQueue:
+			fmt.Println("[ws]sendProc >>>> msg :", string(data))
+			err := node.Conn.WriteMessage(websocket.TextMessage, data)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func receProc(node *Node) {
+	for {
+		_, data, err := node.Conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msg := Message{}
+		err = json.Unmarshal(data, &msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if msg.Type == 3 {
+			currentTime := uint64(time.Now().Unix())
+			node.Heartbeat(currentTime)
+		} else {
+
+		}
+	}
+}
+
+func dispatch(data []byte) {
+	msg := Message{}
+	msg.CreateTime = uint64(time.Now().Unix())
+	err := json.Unmarshal(data, &msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	switch msg.Type {
+	case 1:
+		sendMsg(msg.TargetId, data)
+	case 2:
+		sendGroupMsg(msg.TargetId, data)
+	}
+}
+
+func sendMsg(userId int64, msg []byte) {
+	rwLocker.RLock()
+	node, ok := clientMap[userId]
+	rwLocker.RUnlock()
+	jsonMsg := Message{}
+	json.Unmarshal(msg, &jsonMsg)
+	ctx := context.Background()
+	targetIdStr := strconv.Itoa(int(userId))
+	userIdStr := strconv.Itoa(int(jsonMsg.UserId))
+	jsonMsg.CreateTime = uint64(time.Now().Unix())
+	r, err := utils.Red.Get(ctx, "online_"+userIdStr).Result()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if r != "" {
+		if ok {
+			fmt.Println("sendMsg >>> userID: ", userId, "  msg:", string(msg))
+			node.DataQueue <- msg
+		}
+	}
+	var key string
+	if userId > jsonMsg.UserId {
+		key = "msg_" + userIdStr + "_" + targetIdStr
+	} else {
+		key = "msg_" + targetIdStr + "_" + userIdStr
+	}
+	res, err := utils.Red.ZRevRange(ctx, key, 0, -1).Result()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	score := float64(cap(res)) + 1
+	ress, e := utils.Red.ZAdd(ctx, key, &redis.Z{score, msg}).Result()
+	if e != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(ress)
+}
+
+func sendGroupMsg(targetId int64, msg []byte) {
+	userIds := SearchUserByGroupId(uint(targetId))
 }
